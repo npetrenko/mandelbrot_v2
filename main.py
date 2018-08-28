50471db917b19b45e9e55cc24098ecff2b8c4d7b
Nikita Petrenko <npetrenko97@gmail.com>
2018-08-28 23:49:02 +0300

    updated gui

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class ImageHolder:
     def __init__(self):
         self.ll_corner = np.array([-1,-1], dtype=np.double)
         self.ur_corner = np.array([1,1], dtype=np.double)
+        self.max_iter = 200
         self.update_image()
 
     def get_center(self):
@@ -21,11 +22,15 @@ class ImageHolder:
         dims = self.ur_corner - self.ll_corner
         return dims
 
-    def zoom(self):
+    def zoom(self, direction):
         multiplier = 1.2
         center = self.get_center()
         dims = self.get_dims()
-        dims = dims/multiplier
+
+        if direction == 1:
+            dims = dims/multiplier
+        else:
+            dims *= multiplier
 
         ll_corner = center - dims/2
         ur_corner = center + dims/2
@@ -48,7 +53,10 @@ class ImageHolder:
         self.update_image()
 
     def update_image(self):
-        self.image = create_image(400, 400, tuple(self.ll_corner), tuple(self.ur_corner), 200)
+        self.image = create_image(128, 128, tuple(self.ll_corner), tuple(self.ur_corner), self.max_iter)
+        self.image = self.image.astype(np.double).T
+        self.image /= np.max(self.image)/255
+        self.image = self.image.astype(np.uint8)
         
 
 class app(tk.Frame):
@@ -66,22 +74,48 @@ class app(tk.Frame):
         subf = tk.Frame(self)
         subf.pack()
 
-        qt = tk.Button(subf, text='l', command=lambda x: self.move(x, [-1,0]))
+        qt = tk.Button(subf, text='l', command=lambda: self.move([-1,0]))
         qt.pack(side='left')
 
-        res = tk.Button(subf, text='r', command=lambda x: self.move(x, [1,0]))
+        res = tk.Button(subf, text='r', command=lambda: self.move([1,0]))
         res.pack(side='left')
 
-        qt = tk.Button(subf, text='u', command=lambda x: self.move(x, [0,1]))
+        qt = tk.Button(subf, text='u', command=lambda: self.move([0,-1]))
         qt.pack(side='left')
 
-        res = tk.Button(subf, text='d', command=lambda x: self.move(x, [0,-1]))
+        res = tk.Button(subf, text='d', command=lambda: self.move([0,1]))
         res.pack(side='left')
+
+        qt = tk.Button(subf, text='i', command=lambda: self.zoom(1))
+        qt.pack(side='right')
+
+        res = tk.Button(subf, text='o', command=lambda: self.zoom(-1))
+        res.pack(side='right')
+
+        qt = tk.Button(subf, text='incr', command=lambda: self.incr())
+        qt.pack(side='right')
+
+        res = tk.Button(subf, text='decr', command=lambda: self.decr())
+        res.pack(side='right')
 
         self.reset_im_frame()
 
-    def move(self, event, direction):
+    def incr(self):
+        self.imageholder.max_iter += 100
+        self.reset_im_frame()
+
+    def decr(self):
+        self.imageholder.max_iter -= 100
+        self.imageholder.max_iter = max(self.imageholder.max_iter, 50)
+        self.reset_im_frame()
+
+    def move(self, direction):
         self.imageholder.shift(direction)
+        self.reset_im_frame()
+
+    def zoom(self, direction):
+        self.imageholder.zoom(direction)
+        self.reset_im_frame()
         
     def reset_im_frame(self):
         if hasattr(self, 'im_frame'):
@@ -90,16 +124,16 @@ class app(tk.Frame):
         im_frame = tk.Frame(self)
         im_frame.pack(side='bottom', fill='both', expand='yes')
         self.im_frame = im_frame
-        self.photos = []
-        for image in images[users[self.i]]:
-            self.show_image(os.path.join(users[self.i], image))
+        self.show_image()
 
     def show_image(self):
-        img = Image.fromarray(self.imageholder.image)
+        image = Image.fromarray(self.imageholder.image)
+        image = image.resize((800,800), Image.ANTIALIAS)
         photo = ImageTk.PhotoImage(image)
+        self.photo = photo
 
         image = tk.Label(self.im_frame, image=photo)
-        image.grid(column=0, row=0)
+        image.pack()
 
     def b1(self, event):
         answers[users[self.i]] = 1
